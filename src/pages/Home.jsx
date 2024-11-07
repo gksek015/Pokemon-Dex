b563f66fda367dffda2f7a515a26f058a37b464c
gksekdud <you@example.com>
2024-11-07 17:05:59 +0900

    Home css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,16 @@
 // import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import styled from 'styled-components';
+
+const HomeContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    background-color: #C8C8FF;
+`
 
 const Home = () => {
     const navigate = useNavigate();
@@ -9,10 +20,10 @@ const Home = () => {
     }
 
   return (
-    <div>
+    <HomeContainer>
         <h1>나만의 포켓몬 도감</h1>
         <button onClick={handleStartButton}>시작하기</button>
-    </div>
+    </HomeContainer>
   )
 }
 
